b0ef858001c836209cb62c54b5ab698f39b9bd60
uttamchandrawanshi <140879031+uttamchandrawanshi@users.noreply.github.com>
2023-10-23 09:00:58 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
 import colors from "colors";
+import dotenv from "dotenv";
+
+const PORT = process.env.PORT || 8080;
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
+    app.listen(PORT, () => {
+   console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
+
     console.log(
       `connect to mongodb database ${conn.connection.host}`.bgMagenta.white
     );
