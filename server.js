853493ebad31b6e22438d0d66aac5323491ac39f
uttamchandrawanshi <140879031+uttamchandrawanshi@users.noreply.github.com>
2023-10-23 08:57:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,9 +43,3 @@ app.use("*", function (req, res) {
 const PORT = process.env.PORT || 8080;
 
 //run listen
-app.listen(PORT, () => {
-   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
-  );
-});
