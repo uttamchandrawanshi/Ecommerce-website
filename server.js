af59e4f88954e9b92db1db3b820a807d3507de7d
uttamchandrawanshi <140879031+uttamchandrawanshi@users.noreply.github.com>
2023-10-23 09:22:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //port
 const PORT = process.env.PORT || 8080;
